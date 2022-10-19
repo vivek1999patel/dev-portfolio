2f8bf9e33397dc40198de92afa989a58a06600dc
Vivek Patel <43326066+vivek1999patel@users.noreply.github.com>
2022-10-19 15:25:08 -0400

    Updates

---
+++ static/js/84.4f08fa4d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio_app=self.webpackChunkportfolio_app||[]).push([[84],{7084:function(e,a,r){r.r(a);r(2791),r(3508);var t=r(184);a.default=function(e){var a=e.title;return(0,t.jsx)("div",{className:"header",children:a})}}}]);
+//# sourceMappingURL=84.4f08fa4d.chunk.js.map
